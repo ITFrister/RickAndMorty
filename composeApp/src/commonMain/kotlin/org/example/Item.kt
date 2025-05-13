085def4a3e4275c7ad2e3611e24dbf492f2e3965
ITFrister <it.frister@gmail.com>
2025-05-13 11:00:06 +0300

    wip

---
+++ composeApp/src/commonMain/kotlin/org/example/Item.kt
@@ -0,0 +1,9 @@
+package org.example
+
+import androidx.compose.runtime.Composable
+import com.example.domain.model.CharacterItem
+
+@Composable
+fun CharacterItemWidget(character: CharacterItem) {
+        
+}
